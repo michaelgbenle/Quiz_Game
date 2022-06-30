7af53a0f0c138fad6ca8ae6aba73cde884531449
Michael <michaelgbenle@gmail.com>
2022-07-01 00:27:42 +0100

    working

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	csvFilename := flag.String("csv", "questions.csv", "a csv file for questions and answers")
-	timeLimit := flag.Int("limit", 25, "")
+	timeLimit := flag.Int("limit", 25, "time limit for your question")
 	flag.Parse()
 	file, err := os.Open(*csvFilename)
 	if err != nil {
