db49d195a59d70383924a4d8511a562b114b1fd4
Michael <michaelgbenle@gmail.com>
2022-07-01 00:20:57 +0100

    working

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func parseLines(lines [][]string) []problem {
 	for i, line := range lines {
 		ret[i] = problem{
 			question: line[0],
-			answer:   line[1],
+			answer:   strings.trimspace(line[1]),
 		}
 	}
 	return ret
